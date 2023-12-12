aee3c702c8e21f28bbecc00a37a6e80e8ddf9bfb
joba14 <57039383+4bstr4ct@users.noreply.github.com>
2023-12-12 22:52:50 +0200

    added todos in c_common

--- mirac/include/mirac/c_common.h
+++ mirac/include/mirac/c_common.h
@@ -18,50 +18,62 @@
 #include <stdint.h>
 #include <limits.h>
 
+// TODO: document!
 void* mirac_c_malloc(
 	const uint64_t size);
 
+// TODO: document!
 void* mirac_c_realloc(
 	void* pointer,
 	const uint64_t size);
 
+// TODO: document!
 void mirac_c_free(
 	const void* pointer);
 
+// TODO: document!
 void mirac_c_exit(
 	const int32_t code);
 
+// TODO: document!
 void mirac_c_memset(
 	void* const pointer,
 	const uint8_t value,
 	const uint64_t length);
 
+// TODO: document!
 void mirac_c_memcpy(
 	void* const destination,
 	const void* const source,
 	const uint64_t length);
 
+// TODO: document!
 int32_t mirac_c_memcmp(
 	const uint8_t* const left,
 	const uint8_t* const right,
 	const uint64_t length);
 
+// TODO: document!
 uint64_t mirac_c_strlen(
 	const char* const cstring);
 
+// TODO: document!
 int32_t mirac_c_strcmp(
 	const char* const left,
 	const char* const right);
 
+// TODO: document!
 int32_t mirac_c_strncmp(
 	const char* const left,
 	const char* const right,
 	const uint64_t length);
 
+// TODO: document!
 char* mirac_c_strchr(
 	const char* const string,
 	const int32_t c);
 
+// TODO: document!
 void* mirac_c_bsearch(
 	const void* key,
 	const void* base,
