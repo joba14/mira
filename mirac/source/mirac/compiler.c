c818f56586bc077c470ec8e7dcc452e78fa799a9
joba14 <57039383+joba14@users.noreply.github.com>
2023-12-29 22:59:11 +0000

    ...

--- mirac/source/mirac/compiler.c
+++ mirac/source/mirac/compiler.c
@@ -97,7 +97,296 @@ static void compile_ast_block_expr(
 {
 	mirac_debug_assert(compiler != NULL);
 	mirac_debug_assert(expr_block != NULL);
-	// ... 
+
+	switch (expr_block->token.type)
+	{
+		case mirac_token_type_reserved_lnot:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_land:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_lor:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_lxor:
+		{
+			// TODO: implement!
+		} break;
+
+
+		case mirac_token_type_reserved_bnot:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_band:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_bor:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_bxor:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_shl:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_shr:
+		{
+			// TODO: implement!
+		} break;
+
+
+		case mirac_token_type_reserved_eq:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_neq:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_gt:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_gteq:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_ls:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_lseq:
+		{
+			// TODO: implement!
+		} break;
+
+
+		case mirac_token_type_reserved_add:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_inc:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_sub:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_dec:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_mul:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_div:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_mod:
+		{
+			// TODO: implement!
+		} break;
+
+
+		case mirac_token_type_reserved_ld08:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_ld16:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_ld32:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_ld64:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_st08:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_st16:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_st32:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_st64:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_sys1:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_sys2:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_sys3:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_sys4:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_sys5:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_sys6:
+		{
+			// TODO: implement!
+		} break;
+
+
+		case mirac_token_type_reserved_drop:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_dup:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_over:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_rot:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_reserved_swap:
+		{
+			// TODO: implement!
+		} break;
+
+
+		case mirac_token_type_literal_i08:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_literal_i16:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_literal_i32:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_literal_i64:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_literal_u08:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_literal_u16:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_literal_u32:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_literal_u64:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_literal_f32:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_literal_f64:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_literal_ptr:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_literal_str:
+		{
+			// TODO: implement!
+		} break;
+
+		case mirac_token_type_identifier:
+		{
+			// TODO: implement!
+		} break;
+
+
+		default:
+		{
+			// TODO: implement!
+		} break;
+	}
 }
 
 static void compile_ast_block_as(
@@ -115,7 +404,11 @@ static void compile_ast_block_scope(
 {
 	mirac_debug_assert(compiler != NULL);
 	mirac_debug_assert(scope_block != NULL);
-	// ... 
+
+	for (uint64_t block_index = 0; block_index < scope_block->blocks.count; ++block_index)
+	{
+		compile_ast_block(compiler, &scope_block->blocks.data[block_index]);
+	}
 }
 
 static void compile_ast_block_if(
@@ -124,7 +417,8 @@ static void compile_ast_block_if(
 {
 	mirac_debug_assert(compiler != NULL);
 	mirac_debug_assert(if_block != NULL);
-	// ... 
+
+	// TODO: implement!
 }
 
 static void compile_ast_block_elif(
@@ -133,7 +427,8 @@ static void compile_ast_block_elif(
 {
 	mirac_debug_assert(compiler != NULL);
 	mirac_debug_assert(elif_block != NULL);
-	// ... 
+
+	// TODO: implement!
 }
 
 static void compile_ast_block_else(
@@ -142,7 +437,8 @@ static void compile_ast_block_else(
 {
 	mirac_debug_assert(compiler != NULL);
 	mirac_debug_assert(else_block != NULL);
-	// ... 
+
+	// TODO: implement!
 }
 
 static void compile_ast_block_loop(
@@ -151,7 +447,8 @@ static void compile_ast_block_loop(
 {
 	mirac_debug_assert(compiler != NULL);
 	mirac_debug_assert(loop_block != NULL);
-	// ... 
+
+	// TODO: implement!
 }
 
 static void compile_ast_block_func(
@@ -160,7 +457,8 @@ static void compile_ast_block_func(
 {
 	mirac_debug_assert(compiler != NULL);
 	mirac_debug_assert(func_block != NULL);
-	// ... 
+
+	// TODO: implement!
 }
 
 static void compile_ast_block_mem(
@@ -169,7 +467,8 @@ static void compile_ast_block_mem(
 {
 	mirac_debug_assert(compiler != NULL);
 	mirac_debug_assert(mem_block != NULL);
-	// ... 
+
+	// TODO: implement!
 }
 
 static void compile_ast_block(
