6d6faccfea38a892917824c2d923cebed6c60dc9
joba14 <57039383+4bstr4ct@users.noreply.github.com>
2024-01-02 20:06:36 +0200

    cleanup

--- mirac/scripts/experiments/run.sh
+++ mirac/scripts/experiments/run.sh
@@ -1,10 +1,11 @@
 
 # !/bin/sh
 
-# clear
-# echo "-------------------"
-# ./../build.sh debug
-# time ./../../build/mirac -e _start -a fasm_x86_64_linux ./hello_world.mira ./output.asm
+tabs 4
+clear
+echo "-------------------"
+./../build.sh debug
+time ./../../build/mirac -e _start -a fasm_x86_64_linux ./hello_world.mira ./output.asm
 echo "-------------------"
 nasm -f elf64 ./output.asm -o ./output.o
 ld ./output.o -o ./output.out
