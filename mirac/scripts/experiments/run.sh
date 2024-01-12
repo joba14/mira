d6ffc39946fb2b080656ee1269e0ebe0d3951236
joba14 <57039383+4bstr4ct@users.noreply.github.com>
2024-01-12 13:28:51 +0200

    updating testing scripts and test case

--- mirac/scripts/experiments/run.sh
+++ mirac/scripts/experiments/run.sh
@@ -4,28 +4,28 @@
 tabs 4
 clear
 
+rm -fr ./*.asm
+rm -fr ./*.o
+rm -fr ./*.out
 
-echo "--- debug : building   ---"
+echo "--- debug : building ---"
 ./../build.sh debug
-time ./../../build/mirac_debug -e _start -a nasm_x86_64_linux ./hello_world.mira ./output.asm
+cp -f ./../../build/mirac_debug ./mirac_debug.out
+time ./mirac_debug.out -e _start -a nasm_x86_64_linux ./hello_world.mira ./output_debug.asm
+nasm -f elf64 ./output_debug.asm -o ./output_debug.o
+ld ./output_debug.o -o ./output_debug.out
 
-echo "--- debug : assembling ---"
-nasm -f elf64 ./output.asm -o ./output.o
-ld ./output.o -o ./output.out
+echo "--- release : building ---"
+./../build.sh release
+cp -f ./../../build/mirac_release ./mirac_release.out
+time ./mirac_release.out -e _start -a nasm_x86_64_linux ./hello_world.mira ./output_release.asm
+nasm -f elf64 ./output_release.asm -o ./output_release.o
+ld ./output_release.o -o ./output_release.out
 
-echo "--- debug : running    ---"
-./output.out
+echo "--- debug : running ---"
+time ./output_debug.out
 echo $?
 
-
-echo "--- release : building   ---"
-./../build.sh release
-time ./../../build/mirac_release -e _start -a nasm_x86_64_linux ./hello_world.mira ./output.asm
-
-echo "--- release : assembling ---"
-nasm -f elf64 ./output.asm -o ./output.o
-ld ./output.o -o ./output.out
-
-echo "--- release : running    ---"
-./output.out
+echo "--- release : running ---"
+time ./output_release.out
 echo $?
